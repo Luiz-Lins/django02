6eccf3b698c5a291f6db0a1d2360cb4420c8ea4c
luiz <luizlins22@gmail.com>
2023-03-17 09:21:45 -0300

    Configurado secrets, database com postgres e criação do arquivo flake8.

--- pypro/settings.py
+++ pypro/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-81ja+p^ompjjc*p8+!$p(c_!nqg5k!xuyv9zvoct%d3ae*i8!j'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
